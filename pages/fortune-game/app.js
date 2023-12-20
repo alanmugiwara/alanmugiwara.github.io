a1237451808f1f6bc8d54a5a755c2b682fde33cf
Alan Cruz <38691133+alanmugiwara@users.noreply.github.com>
2023-12-20 17:38:29 -0300

    Update app.js

--- pages/fortune-game/app.js
+++ pages/fortune-game/app.js
@@ -1,21 +1,22 @@
-alert('Boas vindas ao >Fortune Game<\nO jogo do número da sorte!\nUm Jogo simples escrito em Java Script.');
+alert("Boas vindas ao >Fortune Game<\nO jogo do número da sorte!\nUm Jogo simples escrito em Java Script.");
 alert('Caso queira sair do jogo digite "sair"\nna barra de informações.');
 let numeroSecreto = 15;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
 let chute;
 let tentativa = 1;
 
 while (chute != numeroSecreto) {
-  chute = prompt('Por favor, escolha um número entre 1 e 30.');
+  chute = prompt("Por favor, escolha um número entre 1 e 30.");
   if (numeroSecreto == chute) {
-    alert(`Parabéns! Você descobriu o número secreto!\nQue é ${numeroSecreto}! Em ${tentativa} tentativas, miseravi!`);
-  }
-  else {
-    if (chute == "sair" || "SAIR" || "Sair") {
-      alert('Você saiu do jogo.');
+    alert(
+      `Parabéns! Você descobriu o número secreto!\nQue é ${numeroSecreto}! Em ${tentativa} tentativas, miseravi!`
+    );
+  } else {
+    if (chute.toLowerCase() === 'sair') {
+      alert("Você saiu do jogo.");
       break;
-    }
-    else {
+
+    } else {
       if (chute > numeroSecreto) {
         prompt("Acho que você não leu.\nVocê digitou um número\nmaior que 30!");
       } else {
