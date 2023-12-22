4636f000f88e692b74c42c876801507746d2491d
Alan Cruz <38691133+alanmugiwara@users.noreply.github.com>
2023-12-22 16:51:24 -0300

    Update app.js

--- pages/fortune-game/app.js
+++ pages/fortune-game/app.js
@@ -1,4 +1,45 @@
 alert("Boas vindas ao >Fortune Game<\nO jogo do número da sorte!\nUm Jogo simples escrito em Java Script.");
+let numeroSecreto = parseInt(Math.random() * 10 + 1)
+console.log(numeroSecreto);
+let chute;
+let tentativa = 1;
+let maxNum = 10;
+
+
+while (chute != numeroSecreto) {
+  chute = prompt(`Por favor, escolha um número entre 1 e ${maxNum}.`);
+  if (parseInt(chute) === numeroSecreto) {
+  } else {
+    if (chute > maxNum) {
+      alert(`Acho que você não leu.\nVocê digitou um número\nmaior que ${maxNum}!`);
+    } else {
+      if (chute > numeroSecreto) {
+        alert(`O número secreto na verdade é menor que ${chute}.`);
+      } else {
+        alert(`O número secreto na verdade é maior que ${chute}.`);
+      }
+      tentativa++;
+    }
+  }
+}
+
+let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você descobriu o número secreto!\nQue é ${numeroSecreto}! Em ${tentativa} ${palavraTentativa}!`);
+
+
+/*
+
+===================
+"SAIR" FUNCIONANDO
+===================
+
+if (tentativa > 1 )
+  alert(`Parabéns! Você descobriu o número secreto!\nQue é ${numeroSecreto}! Em ${tentativa} tentativas!`);
+else
+  alert(`Parabéns! Você descobriu o número secreto!\nQue é ${numeroSecreto}! Em ${tentativa} única tentativa, miseravi!`); */
+
+
+/* alert("Boas vindas ao >Fortune Game<\nO jogo do número da sorte!\nUm Jogo simples escrito em Java Script.");
 alert('Caso queira sair do jogo digite "sair"\nna barra de informações.');
 let numeroSecreto = 15;
 console.log(numeroSecreto);
@@ -28,4 +69,4 @@ while (chute != numeroSecreto) {
       }
     }
   }
-}
+} */
