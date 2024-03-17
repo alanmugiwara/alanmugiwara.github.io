d619c4106a708b094fb8c347fea1f022bdf729ea
Alan Cruz <38691133+alanmugiwara@users.noreply.github.com>
2024-03-17 19:12:01 -0300

    Create app.js

---
+++ pages/fortune-game-alpha2/app.js
@@ -0,0 +1,69 @@
+let listaDeNumeroSorteados = [];
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+console.log(numeroSecreto);
+
+function exibirTextoNaTela(tag, texto) {
+    let elemento = document.querySelector(tag);
+    elemento.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+function ExibirWellcome() {
+    exibirTextoNaTela('h1', 'Fortune Game versão Alpha');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+ExibirWellcome();
+
+exibirTextoNaTela('h1', 'Fortune Game versão Alpha');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    //usar crase invertidas para chamar uma template string em vez de aspas ou aspas simples  
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}, Miseravi!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById("reiniciar").disabled = false;
+    }
+    else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('h1', 'O número é menor')
+            exibirTextoNaTela('p', 'Tente outra vez')
+        } else {
+            exibirTextoNaTela('h1', 'O número é maior')
+            exibirTextoNaTela('p', 'Tente outra vez')
+        }
+        // tentativas = tentativas + 1/ (versão mais extensa do )
+        tentativas++;
+        limparCampo()
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * 4 + 1);
+    if (listaDeNumeroSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumeroSorteados.push(numeroEscolhido);
+        console.log(listaDeNumeroSorteados);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function rebootGame() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    ExibirWellcome();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
